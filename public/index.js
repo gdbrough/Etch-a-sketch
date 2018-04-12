0dbd3a5a5120537a06e52c0f5c50434095da30a2
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-12 14:58:35 +0100

    initial commit

---
+++ public/index.js
@@ -0,0 +1,43 @@
+window.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.querySelector("#main-canvas");
+  const context = canvas.getContext("2d")
+  let coords = [];
+
+  canvas.addEventListener("click", function(){
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    coords = [event.layerX - 75, event.layerY -75];
+    context.beginPath();
+    context.moveTo(coords[0], coords[1]);
+    return coords;
+  });
+
+
+  document.addEventListener("keyup", function(event){
+    const keyName = event.key;
+    switch (event.key){
+      case "ArrowRight":
+      if(coords[0] < context.canvas.width - 5){
+        context.lineTo(coords[0] += 5,coords[1]);
+      };
+      break;
+      case "ArrowLeft":
+      if(coords[0] > 5){
+        context.lineTo(coords[0] -= 5,coords[1]);
+      };
+      break;
+      case "ArrowUp":
+      if(coords[1] > 5){
+        context.lineTo(coords[0],coords[1] -= 5);
+      };
+      break;
+      case "ArrowDown":
+      if(coords[1] < context.canvas.height - 5){
+        context.lineTo(coords[0],coords[1] += 5);
+      };
+      break;
+    };
+    context.stroke();
+    return coords;
+  });
+
+});
